cf6d44f9fb4f96feea89e34b9a6d5bb3cc5793ff
[ChocoCat555] <siskagpetrova2008@gmail.com>
2026-06-16 09:50:47 +0300

    Elfcheto veche strelya

--- Elfs VS Goblins!!/Form1.Designer.cs
+++ Elfs VS Goblins!!/Form1.Designer.cs
@@ -29,28 +29,83 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.EnemiesMove = new System.Windows.Forms.Timer(this.components);
+            this.ElfPic = new System.Windows.Forms.PictureBox();
+            this.MoveLeft = new System.Windows.Forms.Timer(this.components);
+            this.MoveRight = new System.Windows.Forms.Timer(this.components);
+            this.MoveUp = new System.Windows.Forms.Timer(this.components);
+            this.MoveDown = new System.Windows.Forms.Timer(this.components);
+            this.ElfProjectileMove = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ElfPic)).BeginInit();
             this.SuspendLayout();
             // 
-            // timer1
+            // EnemiesMove
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.EnemiesMove.Enabled = true;
+            this.EnemiesMove.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ElfPic
+            // 
+            this.ElfPic.Image = global::Elfs_VS_Goblins__.Properties.Resources.melf_wand;
+            this.ElfPic.Location = new System.Drawing.Point(303, 443);
+            this.ElfPic.Name = "ElfPic";
+            this.ElfPic.Size = new System.Drawing.Size(150, 210);
+            this.ElfPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ElfPic.TabIndex = 0;
+            this.ElfPic.TabStop = false;
+            // 
+            // MoveLeft
+            // 
+            this.MoveLeft.Interval = 5;
+            this.MoveLeft.Tick += new System.EventHandler(this.MoveLeft_Tick);
+            // 
+            // MoveRight
+            // 
+            this.MoveRight.Interval = 5;
+            this.MoveRight.Tick += new System.EventHandler(this.MoveRight_Tick);
+            // 
+            // MoveUp
+            // 
+            this.MoveUp.Interval = 5;
+            this.MoveUp.Tick += new System.EventHandler(this.MoveUp_Tick);
+            // 
+            // MoveDown
+            // 
+            this.MoveDown.Interval = 5;
+            this.MoveDown.Tick += new System.EventHandler(this.MoveDown_Tick);
+            // 
+            // ElfProjectileMove
+            // 
+            this.ElfProjectileMove.Enabled = true;
+            this.ElfProjectileMove.Interval = 20;
+            this.ElfProjectileMove.Tick += new System.EventHandler(this.ElfProjectileMove_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 450);
+            this.ClientSize = new System.Drawing.Size(778, 644);
+            this.Controls.Add(this.ElfPic);
+            this.MaximumSize = new System.Drawing.Size(800, 700);
+            this.MinimumSize = new System.Drawing.Size(800, 700);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ElfPic)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer EnemiesMove;
+        private System.Windows.Forms.PictureBox ElfPic;
+        private System.Windows.Forms.Timer MoveLeft;
+        private System.Windows.Forms.Timer MoveRight;
+        private System.Windows.Forms.Timer MoveUp;
+        private System.Windows.Forms.Timer MoveDown;
+        private System.Windows.Forms.Timer ElfProjectileMove;
     }
 }
 
--- Elfs VS Goblins!!/Form1.cs
+++ Elfs VS Goblins!!/Form1.cs
@@ -13,20 +13,34 @@ namespace Elfs_VS_Goblins__
     public partial class Form1 : Form
     {
         Random rand;
-        List <Enemy> Chudovishta;
+        Enemy []Chudovishta;
+        ElfProjectile[] Magiya;
+        public class ElfProjectile
+        {
+            public int speed = 10;
+            public PictureBox Pic;
+            protected int x= 6;
+            protected int y = 15;
+            public ElfProjectile()
+            {
+                Pic = new PictureBox();
+                Pic.Size = new Size(x,y);
+                Pic.BackColor = Color.Purple;
+            }
+        }
         public class Enemy
         {
             public int Speed ;
             public int Health ;
-            public PictureBox picture;
-            public int x;
-            public int y;
+            public PictureBox Pic;
+            protected int x;
+            protected int y;
             public int XP;
             public Enemy(int a, int b)
             {
-                picture = new PictureBox();
-                picture.Location = new Point(a, b);
-                picture.Size = new Size(x, y);
+                Pic = new PictureBox();
+                Pic.Location = new Point(a, b);
+                Pic.Size = new Size(x, y);
             }
         }
         public class Goblin : Enemy
@@ -37,42 +51,201 @@ namespace Elfs_VS_Goblins__
                 y = 80;
                 Health = 2;
                 Speed = 2;
-                picture = new PictureBox();
-                picture.Location = new Point(a, b);
-                picture.Size = new Size(x, y);
-                picture.Image = Image.FromFile("sprites/apple-export.gif"); 
-
+                Pic = new PictureBox();
+                Pic.Location = new Point(a, b);
+                Pic.Size = new Size(x, y);
+                Pic.Image = Image.FromFile("sprites/apple-export.gif"); //vremenno
             }
         }
-        
+        public class Tank : Enemy 
+        { 
+            public Tank(int a, int b):base(a,b)
+            {
+                /*
+                x = ;
+                y = ;
+                Health = ;
+                Speed = ;
+                Pic = new PictureBox();
+                Pic.Location = new Point(a, b);
+                Pic.Size = new Size(x, y);
+                Pic.Image = Image.FromFile("sprites/----");
+                */
+            }
+        }
+        public class Demon : Enemy
+        {
+            public Demon(int a, int b) : base(a, b)
+            {
+                /*
+                x = ;
+                y = ;
+                Health = ;
+                Speed = ;
+                Pic = new PictureBox();
+                Pic.Location = new Point(a, b);
+                Pic.Size = new Size(x, y);
+                Pic.Image = Image.FromFile("sprites/----");
+                */
+            }
+        }
         public class Elf
         {
             public int Health = 1;
             public PictureBox Pic;
+            public int speed = 4;
             public int ShootingSpeed;
         }
+        Elf Igrach = new Elf();
 
         public Form1()
         {
             InitializeComponent();
             rand = new Random();
-            Chudovishta = new List <Enemy>();
-            timer1.Start();
+            Chudovishta = new Enemy[1];
+
+            EnemiesMove.Start();
             Goblin test = new Goblin(rand.Next(20, 450), 10);
-            this.Controls.Add(test.picture);
-            Chudovishta.Add(test);
+            this.Controls.Add(test.Pic);
+            Chudovishta[0] =test;
+
+            Igrach.Pic = ElfPic;
+            Magiya = new ElfProjectile[3];
+            for(int i = 0; i<3;i++)
+            {
+                Magiya[i] = new ElfProjectile();
+                this.Controls.Add(Magiya[i].Pic);
+                Magiya[i].Pic.BringToFront();
+            }
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e) //enemies move
         {
             foreach(var a in Chudovishta)
             {
-                a.picture.Top += a.Speed;
-                if(a.picture.Top>=this.Height)
+                a.Pic.Top += a.Speed;
+                if(a.Pic.Top>=this.Height)
                 {
-                    a.picture.Dispose();
+                    a.Pic.Dispose();
                 }
             }
+        }
+
+        private void MoveDown_Tick(object sender, EventArgs e)
+        {
+            if(Igrach.Pic.Top<300)
+            {
+                Igrach.Pic.Top += Igrach.speed;
+            }
+        }
+
+        private void MoveUp_Tick(object sender, EventArgs e)
+        {
+            if (Igrach.Pic.Top > -45)
+            {
+                Igrach.Pic.Top -= Igrach.speed;
+            }
+        }
+
+        private void MoveRight_Tick(object sender, EventArgs e)
+        {
+            if (Igrach.Pic.Right < 535)
+            {
+                Igrach.Pic.Left += Igrach.speed;
+            }
+        }
+
+        private void MoveLeft_Tick(object sender, EventArgs e)
+        {
+            if (Igrach.Pic.Left > -15)
+            {
+                Igrach.Pic.Left -= Igrach.speed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down|e.KeyCode==Keys.S)
+            {
+                MoveDown.Start();
+            }
+            if (e.KeyCode == Keys.Up | e.KeyCode == Keys.W)
+            {
+                MoveUp.Start();
+            }
+            if (e.KeyCode == Keys.Left | e.KeyCode == Keys.A)
+            {
+                MoveLeft.Start();
+            }
+            if (e.KeyCode == Keys.Right | e.KeyCode == Keys.D)
+            {
+                MoveRight.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down | e.KeyCode == Keys.S)
+            {
+                MoveDown.Stop();
+            }
+            if (e.KeyCode == Keys.Up | e.KeyCode == Keys.W)
+            {
+                MoveUp.Stop();
+            }
+            if (e.KeyCode == Keys.Left | e.KeyCode == Keys.A)
+            {
+                MoveLeft.Stop();
+            }
+            if (e.KeyCode == Keys.Right | e.KeyCode == Keys.D)
+            {
+                MoveRight.Stop();
+            }
+        }
+
+        private void ElfProjectileMove_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Magiya.Length; i++)
+            {
+                if(Magiya[i].Pic.Top>0)
+                {
+                    Magiya[i].Pic.Visible = true;
+                    Magiya[i].Pic.Top -= Magiya[i].speed;
+
+                    Collisions();
+                    if(!Igrach.Pic.Visible)
+                    {
+                        GameStop();
+                    }
+                }
+                else
+                {
+                    Magiya[i].Pic.Visible = false;
+                    Magiya[i].Pic.Location = new Point(Igrach.Pic.Location.X+60, Igrach.Pic.Location.Y +70-i*15);
+                }
+            }
+        }
+        private void Collisions()
+        {
+            for(int i = 0;i<Chudovishta.Length;i++)
+            {
+                for(int j = 0; j<Magiya.Length;j++)
+                {
+                    if (Magiya[j].Pic.Bounds.IntersectsWith(Chudovishta[i].Pic.Bounds))
+                    {
+                        Chudovishta[i].Pic.Location = new Point(50, -100);
+                    }
+                }
+                if (Igrach.Pic.Bounds.IntersectsWith(Chudovishta[i].Pic.Bounds))
+                {
+                    Igrach.Pic.Visible = false;
+                }
+            }
+        }
+        private void GameStop()
+        {
+            EnemiesMove.Stop();
+            ElfProjectileMove.Stop();
         }
     }
 }
